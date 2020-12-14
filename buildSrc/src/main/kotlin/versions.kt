e15f455bf523e2ba90823d54d74b6c20381e8ba3
andylamax <andylamax@programmer.net>
2020-12-14 04:52:12 +0300

    initial commit

---
+++ buildSrc/src/main/kotlin/versions.kt
@@ -0,0 +1,13 @@
+object vers {
+    object asoft {
+        val color = "0.0.1"
+        val theme = "0.0.4"
+    }
+
+    object wrappers {
+        val react = "17.0.0-pre.129-kotlin-1.4.20"
+        val styled = "5.2.0-pre.129-kotlin-1.4.20"
+        val css = "1.0.0-pre.129-kotlin-1.4.20"
+        val react_router_dom = "5.2.0-pre.129-kotlin-1.4.20"
+    }
+}
